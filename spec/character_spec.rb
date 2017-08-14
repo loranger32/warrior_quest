6c128f061e5b19f136fb7f4ef9d6a6636ac1c77a
loranger32 <laurentguinotte@gmail.com>
2017-08-14 15:41:59 +0200

    Add specs for attack and damage points

--- spec/character_spec.rb
+++ spec/character_spec.rb
@@ -162,45 +162,171 @@ describe 'A Character' do
   end
 
   context 'when attacking with bare hands' do
+    
     before do
       @attacker = Character.new(name: 'attacker')
       @defender = Character.new(name: 'defender')
     end
 
-    it 'reduce hp points of the defender' do
+    it 'reduces hp points of the defender accordingly' do
       expect(@defender.hp).to eq(100)
       @attacker.attack(@defender)
       expect(@defender.hp).to be_between(76, 90)
     end
   end
 
-  context 'can inflict distinct damage points' do
-
-    before do
-    @attacker = Character.new(name: 'attacker', weapon: :sword)
-    @defender = Character.new(name: 'defender')
-    end
-
-    it 'with a sword' do
-      expect(@attacker.damage_points)
-      .to be_between((8 + 2 + 15), (8 + 16 + 15))
-    end 
-
-  end
-
   context 'when attacking with a sword' do
+
     before do
       @attacker = Character.new(name: 'attacker', weapon: :sword)
       @defender = Character.new(name: 'defender')
+      basic_damage_with_sword = 
+        @attacker.strength + @attacker.weapon.attack_bonus
+      # two dies of 8 with 16 as result
+      @max_damage_with_sword = basic_damage_with_sword + 16
+      # two dies of 8 with 2 as result 
+      @min_damage_with_sword = basic_damage_with_sword + 2  
     end
 
-      it 'inflict sword damages' do
-        @attacker.attack(@defender)
-        expect(@defender.hp)
-        .to be_between(
-          (8 + @attacker.throw_die_of_8_times(2) + 15 ),
-          (@defender.hp + 2))
-      end
+    it 'has a sword' do 
+      expect(@attacker.weapon).to be_instance_of(Sword)
+    end
 
+    it 'has damage points of a sword' do
+      expect(@attacker.damage_points)
+      .to be_between(@min_damage_with_sword, @max_damage_with_sword)
+    end
+
+    it 'reduces hp points of the defender accordingly' do
+      starting_hp_points = @defender.hp
+      @attacker.attack(@defender)
+      expect(@defender.hp)
+      .to be_between(starting_hp_points - @max_damage_with_sword,
+        starting_hp_points - @min_damage_with_sword)
+    end 
+  end
+
+  context 'when attacking with a staff' do
+
+    before do
+      @attacker = Character.new(name: 'attacker', weapon: :staff)
+      @defender = Character.new(name: 'defender')
+      basic_damage_with_staff = 
+        @attacker.strength + @attacker.weapon.attack_bonus
+      # two dies of 8 with 16 as result
+      @max_damage_with_staff = basic_damage_with_staff + 16
+      # two dies of 8 with 2 as result 
+      @min_damage_with_staff = basic_damage_with_staff + 2  
+    end
+
+    it 'has a staff' do 
+      expect(@attacker.weapon).to be_instance_of(Staff)
+    end
+
+    it 'has damage points of a staff' do
+      expect(@attacker.damage_points)
+      .to be_between(@min_damage_with_staff, @max_damage_with_staff)
+    end
+
+    it 'reduces hp points of the defender accordingly' do
+      starting_hp_points = @defender.hp
+      @attacker.attack(@defender)
+      expect(@defender.hp)
+      .to be_between(starting_hp_points - @max_damage_with_staff,
+        starting_hp_points - @min_damage_with_staff)
+    end 
+  end
+
+  context 'when attacking with a spear' do
+
+    before do
+      @attacker = Character.new(name: 'attacker', weapon: :spear)
+      @defender = Character.new(name: 'defender')
+      basic_damage_with_spear = 
+        @attacker.strength + @attacker.weapon.attack_bonus
+      # two dies of 8 with 16 as result
+      @max_damage_with_spear = basic_damage_with_spear + 16
+      # two dies of 8 with 2 as result 
+      @min_damage_with_spear = basic_damage_with_spear + 2  
+    end
+
+    it 'has a spear' do 
+      expect(@attacker.weapon).to be_instance_of(Spear)
+    end
+
+    it 'has damage points of a spear' do
+      expect(@attacker.damage_points)
+      .to be_between(@min_damage_with_spear, @max_damage_with_spear)
+    end
+
+    it 'reduces hp points of the defender accordingly' do
+      starting_hp_points = @defender.hp
+      @attacker.attack(@defender)
+      expect(@defender.hp)
+      .to be_between(starting_hp_points - @max_damage_with_spear,
+        starting_hp_points - @min_damage_with_spear)
+    end 
+  end
+
+  context 'when attacking with a two hands axe' do
+
+    before do
+      @attacker = Character.new(name: 'attacker', weapon: :axe)
+      @defender = Character.new(name: 'defender')
+      basic_damage_with_axe = 
+        @attacker.strength + @attacker.weapon.attack_bonus
+      # two dies of 8 with 16 as result
+      @max_damage_with_axe = basic_damage_with_axe + 16
+      # two dies of 8 with 2 as result 
+      @min_damage_with_axe = basic_damage_with_axe + 2  
+    end
+
+    it 'has an axe' do 
+      expect(@attacker.weapon).to be_instance_of(Axe)
+    end
+
+    it 'has damage points of a axe' do
+      expect(@attacker.damage_points)
+      .to be_between(@min_damage_with_axe, @max_damage_with_axe)
+    end
+
+    it 'reduces hp points of the defender accordingly' do
+      starting_hp_points = @defender.hp
+      @attacker.attack(@defender)
+      expect(@defender.hp)
+      .to be_between(starting_hp_points - @max_damage_with_axe,
+        starting_hp_points - @min_damage_with_axe)
+    end 
+  end
+
+  context 'when attacking with a short sword' do
+
+    before do
+      @attacker = Character.new(name: 'attacker', weapon: :short_sword)
+      @defender = Character.new(name: 'defender')
+      basic_damage_with_short_sword = 
+        @attacker.strength + @attacker.weapon.attack_bonus
+      # two dies of 8 with 16 as result
+      @max_damage_with_short_sword = basic_damage_with_short_sword + 16
+      # two dies of 8 with 2 as result 
+      @min_damage_with_short_sword = basic_damage_with_short_sword + 2  
+    end
+
+    it 'has a short_sword' do 
+      expect(@attacker.weapon).to be_instance_of(ShortSword)
+    end
+
+    it 'has damage points of a short_sword' do
+      expect(@attacker.damage_points)
+      .to be_between(@min_damage_with_short_sword, @max_damage_with_short_sword)
+    end
+
+    it 'reduces hp points of the defender accordingly' do
+      starting_hp_points = @defender.hp
+      @attacker.attack(@defender)
+      expect(@defender.hp)
+      .to be_between(starting_hp_points - @max_damage_with_short_sword,
+        starting_hp_points - @min_damage_with_short_sword)
+    end 
   end
 end
