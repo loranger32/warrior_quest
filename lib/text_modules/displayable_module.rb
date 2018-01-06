e695ff8c2489e24efe7568b6abf4867229899aea
Laurent G <laurentguinotte@gmail.com>
2018-01-06 16:41:19 +0100

    Add possibility to display rules for choosing a name

--- lib/text_modules/displayable_module.rb
+++ lib/text_modules/displayable_module.rb
@@ -1,5 +1,19 @@
 # A utils module to be included in game_class but defined separately for clarity
+# In order to get its methods available in other modules, I've duplicated all
+# instance methods in module methods - maybe not the more convenient way to do.
 module Displayable
+  def self.print_message(message)
+    puts.message.blue
+  end
+
+  def self.prompt(message)
+    puts "=> #{message}".green
+  end
+
+  def self.clear_screen
+    (system 'clear') || (system 'cls')
+  end
+
   def print_message(message)
     puts message.blue
   end
