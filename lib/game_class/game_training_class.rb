ae610a1b115fba2e105db75ed136974f4aabaf63
Laurent G <laurentguinotte@gmail.com>
2018-01-08 09:37:45 +0100

    Add basic initialization method for Training class + remove #sleep for developpement

--- lib/game_class/game_training_class.rb
+++ lib/game_class/game_training_class.rb
@@ -1,5 +1,23 @@
 class Training
-  def play
-    puts "Welcome to training !"
+  include Rollable
+  include Textable
+  include Displayable
+  include Validable
+
+  attr_reader :player, :teamates
+
+  def initialize
+    @player   = Warrior.create_hero
+    @teamates = set_teamates
+  end
+
+  private
+
+  def set_teamates
+    team = []
+    team << Dwarf.create_passipti
+    team << Elve.create_toudou
+    team << Wizard.create_hocus_pocus
+    team
   end
 end
